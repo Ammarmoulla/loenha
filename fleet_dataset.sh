07b24e1441f2a2ac52359baae5580ba4f08729ab
Ammar moulla <ammarturing@gmail.com>
2024-04-11 12:31:59 +0300

    update script dataset :hammer:

--- fleet_dataset.sh
+++ fleet_dataset.sh
@@ -1,7 +1,7 @@
-!unzip /content/shakkelha/dataset/extra_train.zip
-!mkdir -p data
-!tail -n 10000 extra_train.txt > dataset.txt
-!split -l 9000 dataset.txt
-!mv xaa train.txt
-!mv xab valid.txt
-!mv train.txt valid.txt loenha/data
+unzip /content/shakkelha/dataset/extra_train.zip
+mkdir -p data
+tail -n 10000 extra_train.txt > dataset.txt
+split -l 9000 dataset.txt
+mv xaa train.txt
+mv xab valid.txt
+mv train.txt valid.txt loenha/data
