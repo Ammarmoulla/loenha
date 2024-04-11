a3fbcfdafe35b113deb0bf869cb3c51904c9654e
Ammar moulla <ammarturing@gmail.com>
2024-04-11 13:29:56 +0300

    update script datasets :rewind:

--- fleet_dataset.sh
+++ fleet_dataset.sh
@@ -1,7 +1,7 @@
 script_path=$(readlink -f "$0")
 script_dir=$(dirname "$script_path")
 unzip /content/shakkelha/dataset/extra_train.zip
-mkdir -p ${script_dir}/data
+mkdir -p data
 tail -n 10000 extra_train.txt > dataset.txt
 split -l 9000 dataset.txt
 mv xaa train.txt
